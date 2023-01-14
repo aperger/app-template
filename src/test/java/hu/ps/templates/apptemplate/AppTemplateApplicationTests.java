71b35b8bcc37e550e72d706d697245c9004c9e50
Attila Perger <attila.perger@klm.com>
2023-01-14 23:42:37 +0100

    Initial commit

---
+++ src/test/java/hu/ps/templates/apptemplate/AppTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.ps.templates.apptemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
