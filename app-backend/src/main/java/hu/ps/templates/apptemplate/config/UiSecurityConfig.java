c99f3808cb3b08d98b6490831db4b5b690d02b9b
Attila Perger <attila.perger@klm.com>
2023-01-16 13:51:48 +0100

    Fixed security and deploy process

--- app-backend/src/main/java/hu/ps/templates/apptemplate/config/UiSecurityConfig.java
+++ app-backend/src/main/java/hu/ps/templates/apptemplate/config/UiSecurityConfig.java
@@ -1,16 +1,32 @@
 package hu.ps.templates.apptemplate.config;
-/*
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * https://medium.com/@bcarunmail/securing-rest-api-using-keycloak-and-spring-oauth2-6ddf3a1efcc2
+ * https://developers.redhat.com/articles/2022/12/07/how-implement-single-sign-out-keycloak-spring-boot#keycloak_configuration
+ */
+@Configuration
 @EnableWebSecurity
 public class UiSecurityConfig {
 
+    @Value("${keycloak.application-url}")
+    private String applicationUrl;
+
+    @Value("${keycloak.realm}")
+    private String keyCloakRealm;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+
+        final String successLogoutUrl = keyCloakRealm + "/protocol/openid-connect/logout?redirect_uri=" + applicationUrl + "/login";
+
+        http.authorizeHttpRequests()
                 .requestMatchers("/favicon.ico").permitAll()
                 .requestMatchers("/*.js").permitAll()
                 .requestMatchers("/*.css").permitAll()
@@ -24,12 +40,15 @@ public class UiSecurityConfig {
                 .requestMatchers("/img/**").permitAll()
                 .requestMatchers("/imgs/**").permitAll()
                 .requestMatchers("/assets/**").permitAll()
-                .requestMatchers("/actuator/health").permitAll()
+                .requestMatchers("/actuator/health**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login();
+                    .oauth2Login()
+                .and()
+                    .logout()
+                .logoutSuccessUrl(successLogoutUrl)
+            ;
         return http.build();
     }
 
 }
-*/
